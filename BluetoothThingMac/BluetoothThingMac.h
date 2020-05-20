fa442cb5dca0c577c4f08da2a2b6d09749735f8f
Antonio Yip <antoniokly@users.noreply.github.com>
2020-05-21 11:34:44 +1200

    add targets

---
+++ BluetoothThingMac/BluetoothThingMac.h
@@ -0,0 +1,19 @@
+//
+//  BluetoothThingMac.h
+//  BluetoothThingMac
+//
+//  Created by Antonio Yip on 21/05/20.
+//  Copyright © 2020 Antonio Yip. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BluetoothThingMac.
+FOUNDATION_EXPORT double BluetoothThingMacVersionNumber;
+
+//! Project version string for BluetoothThingMac.
+FOUNDATION_EXPORT const unsigned char BluetoothThingMacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BluetoothThingMac/PublicHeader.h>
+
+
