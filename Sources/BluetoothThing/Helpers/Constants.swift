37aa71053222f58a7d90779149d4cf58acd663b2
Antonio Yip <antoniokly@users.noreply.github.com>
2020-05-30 16:08:55 +1200

    fix targets

--- Sources/BluetoothThing/Helpers/Constants.swift
+++ Sources/BluetoothThing/Helpers/Constants.swift
@@ -8,6 +8,14 @@
 
 import Foundation
 
+#if canImport(WatchKit)
+import WatchKit
+#endif
+
+#if canImport(TVUIKit)
+import TVUIKit
+#endif
+
 extension String {
     static let id = "id"
     static let centralId = "centralId"
@@ -22,4 +30,35 @@ extension String {
     static let key = "key"
     static let value = "value"
     static let modifiedAt = "modifiedAt"
+}
+
+var deviceName: String {
+    #if os(iOS)
+    return UIDevice.current.name
+    #elseif os(OSX)
+    return Host.current().localizedName!
+    #elseif os(watchOS)
+    return WKInterfaceDevice.current().name
+    #else
+    return "unknown"
+    #endif
+}
+
+var bundle: Bundle {
+    Bundle(identifier: "yip.antonio.BluetoothThing")!
+}
+
+var centralId: UUID {
+    #if os(iOS)
+    return UIDevice.current.identifierForVendor!
+    #else
+    if let uuidString = UserDefaults.standard.object(forKey: .centralId) as? String,
+        let uuid = UUID(uuidString: uuidString) {
+        return uuid
+    } else {
+        let uuid = UUID()
+        UserDefaults.standard.set(uuid.uuidString, forKey: .centralId)
+        return uuid
+    }
+    #endif
 }
